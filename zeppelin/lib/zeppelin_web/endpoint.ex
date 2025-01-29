337b49f71290f2cb2e25178bd884a8fcf5364682
barbosa_arco <natanbarbosa50@gmail.com>
2025-01-29 14:25:49 -0300

    feat: inicio do projeto, com as libs e inicio do controller

---
+++ zeppelin/lib/zeppelin_web/endpoint.ex
@@ -0,0 +1,51 @@
+defmodule ZeppelinWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :zeppelin
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  @session_options [
+    store: :cookie,
+    key: "_zeppelin_key",
+    signing_salt: "j8vZ7uLl"
+  ]
+
+  socket "/socket", ZeppelinWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phx.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/",
+    from: :zeppelin,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    plug Phoenix.CodeReloader
+  end
+
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Phoenix.json_library()
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, @session_options
+  plug ZeppelinWeb.Router
+end
